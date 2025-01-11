4a2d846ebb25a233ffa73e45a4cff4d4efb17dcc
GaladrielLothlorien <galadriel1335@outlook.com>
2025-01-11 20:33:00 -0300

    Versionamento de codigo da funcao soma

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "operation.h"
+
+int main(){
+    int a = 10;
+    int b = 20;
+
+    printf("A soma eh: %d\n", sum(a, b));
+
+    return 0;
+}
---
+++ operation.h
@@ -0,0 +1,7 @@
+#ifndef OPERATION_H
+#define OPERATION_H 
+
+//So chama as funcoes a logica vai no operation.c
+int sum(int x, int y);
+
+#endif
